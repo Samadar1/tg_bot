05d5e975589e2bef7d5df7e0a8bac4d79233be42
ruslanchik2005 <120409696+ruslanchik2005@users.noreply.github.com>
2022-12-12 22:06:54 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -33,10 +33,5 @@ def handle_text(message):
         multiline_str_split_list = sss.rsplit(sep='\n') 
         for s in multiline_str_split_list: 
             bot.send_message(message.chat.id,s)
-print('ыыы')
-
 
 bot.polling(none_stop=True)
-
-def asdasds():
-    print(12376213476)
