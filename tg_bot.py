0a884827b2c52bfdb9f0882867d3bb4385bdeca7
Samadar1 <120409396+Samadar1@users.noreply.github.com>
2022-12-12 21:23:14 +0300

    fd

--- tg_bot.py
+++ tg_bot.py
@@ -13,7 +13,7 @@ f.close()
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    mess = 'ку это бот'
+    mess = '�� ��� ���'
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
@@ -38,6 +38,4 @@ def handle_text(message):
 print('dsdasd')
 bot.polling(none_stop=True)
 
-# руслан был здесь
-# def deadinside():
-#    print("i am deadinside")
+
