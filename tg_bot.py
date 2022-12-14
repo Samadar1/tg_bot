9fcad686d2aa13570d1b7ba964d99cbe07034dcf
Samadar1 <120409396+Samadar1@users.noreply.github.com>
2022-12-14 03:27:33 +0300

    Update tg_bot.py
    РІСЃС‘ СЃРґРµР»Р°Р» РїСЂРѕРІРµСЂРєСѓ РѕС‚РІРµС‚РѕРІ

--- tg_bot.py
+++ tg_bot.py
@@ -1,11 +1,15 @@
 # -*- coding: UTF-8 -*-
+
+from sys import flags
 import telebot
 from telebot import types
 import random
 
 
-
 bot = telebot.TeleBot('5491906660:AAFP9IUsQ6hvPsG4G48CHq2AYk3GdQKnkQc')
+
+rand = 0
+flag = False
 
 
 
@@ -17,14 +21,18 @@ with open('quest.txt', 'r',encoding='UTF-8') as file:
 		nomera_list.append(x_str)
 
 with open('answ.txt', 'r',encoding='UTF-8') as file:
-	answ_list = [str(x) for x in file.readlines()]
+	answ_list = []
+	for x in file.readlines():
+		x_str = str(x)
+		x_str = x_str.strip('\n')
+		answ_list.append(x_str)
 	
+
 		
 @bot.message_handler(commands=["start"])
 def start(message):
 	mess = 'ку это бот по подготовке к огэ. Напиши команду /buttons'
 	bot.send_message(message.chat.id, mess, parse_mode='html')
-
 
 @bot.message_handler(commands=["buttons"])
 def com(message):
@@ -35,10 +43,11 @@ def com(message):
 	markup.add(nomera,variant,teori)
 	bot.send_message(message.chat.id,'выбор категории', reply_markup=markup)
 
-
 @bot.message_handler(content_types=["text"])
 def func(message):
-	global rand
+	global flag
+	global rand 
+
 	if(message.text =='номера'):
 		markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 		nomer1 = types.KeyboardButton('номер 1')
@@ -56,20 +65,33 @@ def func(message):
 		markup.add(nomera_a,variant_a,teori_a)
 		bot.send_message(message.chat.id,'Вы вернулись в главное меню', reply_markup=markup)
 
+	elif flag == False:
+		bot.send_message(message.chat.id,'бот не распознаёт ваше сообщение', parse_mode='html')
 
 	if message.text == 'номер 1':
 		rand = random.randint(0,3)
-		otvet = types.InlineKeyboardMarkup(row_width=1)
-		button1 = types.InlineKeyboardButton("Правильный ответ!", callback_data ='1')
-		otvet.add(button1)
-		bot.send_message(message.chat.id,nomera_list[rand],reply_markup=otvet, parse_mode='html')
-		
-		
+		bot.send_message(message.chat.id,nomera_list[rand], parse_mode='html')
+		bot.send_message(message.chat.id,'введите ответ', parse_mode='html')
+		flag = True
+		return 
+	if flag == True:	 
+		if (message.text == answ_list[rand]):
+			bot.send_message(message.chat.id,'Молодец!')
+			flag = False
+		else:
+			otvet = types.InlineKeyboardMarkup(row_width=1)
+			button1 = types.InlineKeyboardButton("Правильный ответ!", callback_data ='1')
+			otvet.add(button1)
+			bot.send_message(message.chat.id,'Ответ неправильный, если есть трудности посмотри теорию, а также можешь узнать правильный ответ.',reply_markup=otvet,parse_mode='html')
+	
+	
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
+	global flag
 	if call.message:
 		if call.data == "1":
 			bot.send_message(call.message.chat.id,answ_list[rand])
+			flag = False
 		if call.data == "2":
 			bot.send_message(call.message.chat.id,answ_list[rand])
 			
